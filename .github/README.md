233847cd4627f0920a0ba5c6c8c7126545348b45
Devin Humphreys <dhumphreys@urbn.com>
2024-11-25 17:16:57 -0500

    Add HotKeys with cogent

--- .github/README.md
+++ .github/README.md
@@ -6,3 +6,14 @@ parse that into true json and display the output.
 ![Demo Gif](static/demo.gif)
 
 ### NOTE: Arrays or straight text not yet supported
+
+## Hot Keys
+**NOTE**: MacOS tested only
+- `Cmd + q` - Exit
+
+#### Select Input Area
+- `Cmd + Enter` - Submits input
+- `Cmd + Backspace` - Clears input
+
+#### Select Output Area
+- `Cmd + c` - Copied output
--- app/main.go
+++ app/main.go
@@ -5,10 +5,16 @@ import (
     "cogentcore.org/core/core"
     "cogentcore.org/core/events"
     "cogentcore.org/core/styles"
+    "cogentcore.org/core/styles/abilities"
+    "cogentcore.org/core/styles/states"
     "cogentcore.org/core/styles/units"
     "cogentcore.org/core/texteditor"
     "os/exec"
 )
+
+const CmdEnterKeyChord = "Meta+ReturnEnter"
+const CmdBackspaceKeyChord = "Meta+Backspace"
+const CmdCKeyChord = "Meta+C"
 
 func main() {
     b := core.NewBody("JSONify")
@@ -44,7 +50,7 @@ func main() {
     outputHeader.Styler(headerStyler)
 
     outputTextAreaContainer := core.NewFrame(output)
-    outputTextAreaContainer.Styler(inputTextEditorStyler)
+    outputTextAreaContainer.Styler(outputTextContainerStyler)
     outputTextArea := core.NewText(outputTextAreaContainer)
     outputTextArea.Styler(outputTextAreaStyler)
 
@@ -57,7 +63,8 @@ func main() {
     outputCopyBtn := core.NewButton(outputBtnFrame)
 
     // --- Event Handlers
-    inputSubmitBtn.SetText("Submit").OnClick(func(e events.Event) {
+    // TODO - clean this uuuup, do we really need to be so javascripty?
+    onSubmit := func(e events.Event) {
         formattedText, err := parse(inputEditor.Buffer.String())
         if err != nil {
             core.ErrorDialog(b, err, "Unable to parse input")
@@ -66,17 +73,46 @@ func main() {
 
         outputTextArea.SetText(formattedText)
         outputTextArea.Update()
-    })
-    inputClearBtn.SetText("Clear").OnClick(func(e events.Event) {
+    }
+    onClear := func(e events.Event) {
         inputEditor.Buffer.SetText([]byte(""))
-    })
-    outputCopyBtn.SetText("Copy").OnClick(func(e events.Event) {
+    }
+    onCopy := func(e events.Event) {
         err := setToClipboard(outputTextArea.Text)
         if err != nil {
             core.ErrorSnackbar(b, err, "Error copying content")
         }
         core.MessageSnackbar(b, "Output copied")
+    }
+
+    inputSubmitBtn.SetText("Submit").OnClick(onSubmit)
+    inputClearBtn.SetText("Clear").OnClick(onClear)
+    outputCopyBtn.SetText("Copy").OnClick(onCopy)
+
+    // Hot Keys
+    inputEditor.OnKeyChord(func(e events.Event) {
+        if CmdEnterKeyChord == e.KeyChord() {
+            onSubmit(e)
+        }
+        if CmdBackspaceKeyChord == e.KeyChord() {
+            onClear(e)
+        }
     })
+
+    // We've got to do this on `outputTextAreaContainer` and `outputTextArea` because they may push each
+    //other out of the way, and we don't care which they're on really, just that they're in the right-ish place.
+    outputTextAreaContainer.OnKeyChord(func(e events.Event) {
+        if CmdCKeyChord == e.KeyChord() {
+            onCopy(e)
+        }
+    })
+    outputTextArea.OnKeyChord(func(e events.Event) {
+        if CmdCKeyChord == e.KeyChord() {
+            onCopy(e)
+        }
+    })
+
+    // --- Run
     b.RunMainWindow()
 }
 
@@ -94,7 +130,18 @@ func inputTextEditorStyler(s *styles.Style) {
     s.Grow.Set(1, 1)
 }
 
+func outputTextContainerStyler(s *styles.Style) {
+    s.Grow.Set(1, 1)
+    s.SetAbilities(true, abilities.Selectable, abilities.Focusable, abilities.Hoverable)
+}
+
 func outputTextAreaStyler(s *styles.Style) {
+    s.Grow.Set(1, 1)
+    s.Abilities.SetFlag(true, abilities.Focusable)
+    s.MaxBoxShadow = styles.BoxShadow1()
+    if s.Is(states.Hovered) {
+        s.BoxShadow = s.MaxBoxShadow
+    }
     s.Text.WhiteSpace = styles.WhiteSpacePre
 }
 
@@ -102,6 +149,7 @@ func btnRowStyler(s *styles.Style) {
     s.Direction = styles.Row
 }
 
+// TODO - can you use widgetevents.WidgetBase.Clipboard() instead?
 func setToClipboard(content string) error {
     cmd := exec.Command("pbcopy")
     in, err := cmd.StdinPipe()
